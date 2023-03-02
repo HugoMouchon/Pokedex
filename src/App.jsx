61aeeea9c0459d76afba4926d1d5e665c88dd570
Hugo <hugomouchonpro@gmail.com>
2023-03-02 12:51:38 +0100

    Commencement de la mise en place des différentes fonctions permettant de récupérer les données de l'API de ce que l'on veut affficher (image,nom,poids,hauteur...)

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,47 @@ import { pokemonAPI } from './api/pokemonAPI';
 
 export function App() {
 
-  const [imagePokemon, setImagePokemon] = useState([]);
+  const [imagePokemon, setImagePokemon] = useState();
+  const [namePokemon, setNamePokemon] = useState(null);
+  const [weightPokemon, setWeightPokemon] = useState();
+  const [heightPokemon, setHeightPokemon] = useState();
 
   async function fetchPokemonImage(pokemonID) {
     const imageURL = await pokemonAPI.fetchPokemonImage(pokemonID);
-    if (imageURL.length > 0) {
-      setImagePokemon(imageURL);
-    }
+    setImagePokemon(imageURL);
   }
 
   useEffect(() => {
-    fetchPokemonImage(35);
+    fetchPokemonImage(3);
   }, []);
+
+  async function fetchPokemonName(pokemonID) {
+    const name = await pokemonAPI.fetchPokemonName(pokemonID);
+    setNamePokemon(name);
+  }
+
+  useEffect(() => {
+    fetchPokemonName(3);
+  }, [])
+
+  async function fetchPokemonWeight(pokemonID) {
+    const weight = await pokemonAPI.fetchPokemonWeight(pokemonID);
+    setWeightPokemon(weight);
+  }
+
+  useEffect(() => {
+    fetchPokemonWeight(3);
+  }, [])
+
+  async function fetchPokemonHeight(pokemonID) {
+    const height = await pokemonAPI.fetchPokemonHeight(pokemonID);
+    setHeightPokemon(height);
+  }
+
+  useEffect(() => {
+    fetchPokemonHeight(3);
+  }, [])
+
 
   return (
     <div className={style.container}>
@@ -28,7 +57,10 @@ export function App() {
       </div>
 
       <div className={style.details}>
-        <img src={imagePokemon} alt="" />
+        <img className={style.image} src={imagePokemon} alt="" />
+        <h1>Pokemon: {namePokemon}</h1>
+        <h3>Poid: {weightPokemon} kilos</h3>
+        <h3>Hauteur: {heightPokemon} mètres</h3>
       </div>
       <div className={style.statistique}>
         <h1>Coucou</h1>
--- src/api/pokemonAPI.js
+++ src/api/pokemonAPI.js
@@ -5,5 +5,20 @@ export class pokemonAPI {
     static async fetchPokemonImage(pokemonID) {
         const response = await axios.get(`${BASE_URL_POKEAPI}pokemon/${pokemonID}`);
         return response.data.sprites.other.dream_world.front_default;
-    }
+    };
+
+    static async fetchPokemonName(pokemonID) {
+        const response = await axios.get(`${BASE_URL_POKEAPI}pokemon/${pokemonID}`);
+        return response.data.species.name;
+    };
+
+    static async fetchPokemonWeight(pokemonID) {
+        const response = await axios.get(`${BASE_URL_POKEAPI}pokemon/${pokemonID}`);
+        return response.data.weight;
+    };
+
+    static async fetchPokemonHeight(pokemonID) {
+        const response = await axios.get(`${BASE_URL_POKEAPI}pokemon/${pokemonID}`);
+        return response.data.height;
+    };
 }
